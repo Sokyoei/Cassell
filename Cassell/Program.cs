cf28e0c5b32bbec470d83d066c24f0667b852f92
Sokyoei <89757286+Sokyoei@users.noreply.github.com>
2026-08-14 17:28:41 +0800

    update

--- Cassell/Program.cs
+++ Cassell/Program.cs
@@ -1,2 +1,16 @@
-// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+namespace Ahri
+{
+    class Animal
+    {
+        private string Name { get; set; }
+
+        public Animal(string name) { Name = name; }
+
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            Animal animal = new Animal("Sokyoei");
+            Console.WriteLine(animal.Name);
+        }
+    }
+}
